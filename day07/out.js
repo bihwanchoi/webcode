1a926953e0cdfb8c1bae6aec0dbfa83111114eab
bihwanchoi <84217296+bihwanchoi@users.noreply.github.com>
2021-05-29 13:49:48 +0900

    Add files via upload

---
+++ day07/out.js
@@ -0,0 +1,6 @@
+function f1() {
+    alert('함수1을 사용(호출)하였음.')
+}
+function f2() {
+    alert('함수2를 사용(호출)하였음.')
+}
---
+++ day07/out2.js
@@ -0,0 +1,6 @@
+function f3() {
+    alert('함수3을 실행(호출)하였음')
+}
+function f4() {
+    alert('함수4를 실행(호출)하였음')
+}
